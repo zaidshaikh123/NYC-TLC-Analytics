06b810101b2bab8fba4ee8defbaea2eb64e3abfe
Zaid Shaikh <zaid.r.shk@gmail.com>
2026-09-15 14:34:12 -0400

    Update dim_date.sql to create a series of dates from 2025-12-31 to 2026-04-01 to join with interim_trip_enriched model without losing any events

--- nyc_tlc_analytics/models/marts/dim_date.sql
+++ nyc_tlc_analytics/models/marts/dim_date.sql
@@ -1,23 +1,25 @@
 WITH date_spine AS (
     {{dbt_utils.date_spine(
-        start_date = "CAST('2026-01-01' AS date)",
-        end_date = "CAST('2026-03-31' AS date)",
+        start_date = "CAST('2025-12-31' AS DATE)",
+        end_date = "CAST('2026-04-01' AS DATE)",
         datepart = "day"
     )}}
 ),
 
 uuid_date AS (
-    SELECT GENERATE_UUID() AS id, TIMESTAMP(date_day, 'America/New_York') AS date_utc
+    SELECT date_day,
+    TIMESTAMP(date_day, 'America/New_York') AS date_utc_sod,
     FROM date_spine
 )
 
-SELECT *,
-EXTRACT(YEAR FROM date_utc AT TIME ZONE 'America/New_York') AS date_year,
-EXTRACT(MONTH FROM date_utc AT TIME ZONE 'America/New_York') AS date_month,
-EXTRACT(DAY FROM date_utc AT TIME ZONE 'America/New_York') AS date_day,
-FORMAT_TIMESTAMP('%A',date_utc, 'America/New_York') AS date_dow,
+SELECT GENERATE_UUID() AS id, *, TIMESTAMP_ADD(date_utc_sod, INTERVAL 86399 SECOND) AS date_utc_eod,
+EXTRACT(YEAR FROM date_utc_sod AT TIME ZONE 'America/New_York') AS year,
+EXTRACT(MONTH FROM date_utc_sod AT TIME ZONE 'America/New_York') AS month,
+EXTRACT(DAY FROM date_utc_sod AT TIME ZONE 'America/New_York') AS day,
+FORMAT_TIMESTAMP('%A',date_utc_sod, 'America/New_York') AS dow,
 (CASE
-    WHEN EXTRACT(DAYOFWEEK FROM date_utc AT TIME ZONE 'America/New_York') IN (1,7) THEN 'Weekend'
+    WHEN EXTRACT(DAYOFWEEK FROM date_utc_sod AT TIME ZONE 'America/New_York') IN (1,7) THEN 'Weekend'
     ELSE 'Weekday'
 END) AS is_weekday
 FROM uuid_date
+ORDER BY date_utc_sod
